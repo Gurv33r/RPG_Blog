1dd11d770bf49622975db86d492b49c8d509452c
Gurv33r <gurveersingh477@gmail.com>
2022-01-15 18:04:00 -0800

    conditional rendering working

--- frontend/pages/all.js
+++ frontend/pages/all.js
@@ -1,24 +1,35 @@
 function Blog({ posts }) {
-    return (
-      <div className="posts-wrapper">
-        {posts.map((post, i) => (
-          <div className="post" key={i} >
+
+  function jsxify(post, i) {
+    let editDate = post.UpdatedAt
+    if (!editDate.Valid){
+      return (
+        <div className="post" key={i} >
             <div className="publish-date">{post.Date}</div>
             <p className="content">{post.Content}</p>
-            <div className="update-time">{post.UpdatedAt}</div>
-          </div>
-        ))} 
+        </div>
+      )
+    } else {
+      return (
+        <div className="post" key={i} >
+          <div className="publish-date">{post.Date}</div>
+          <p className="content">{post.Content}</p>
+          <div className="edit-date">{editDate.Time}</div>
+        </div>
+      )
+    }
+  }
+
+    return (
+      <div className="posts-wrapper">
+        {posts.map((post, i) => jsxify(post,i))} 
       </div>
     )
   }
 
-  // This function gets called at build time
 export async function getStaticProps() {
-    // Call an external API endpoint to get posts
     const res = await fetch('http://localhost:8080/all')
     const posts = await res.json();
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
     return {
       props: {
          posts
