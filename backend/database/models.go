cd22294af100af7bec859f419c0d1c9202bd832e
Gurv33r <gurveersingh477@gmail.com>
2022-01-16 19:17:01 -0800

    updatedAt bugfix

--- backend/database/models.go
+++ backend/database/models.go
@@ -1,12 +1,13 @@
 package database
 
 import (
-	"database/sql"
 	"time"
+
+	"github.com/go-pg/pg/v10/types"
 )
 
 type Post struct {
-	Date      time.Time    `json:"Date" pg:"default:now(), pk, notnull"`
-	Content   string       `json:"Content" pg:", notnull"`
-	UpdatedAt sql.NullTime `json:"UpdatedAt,omitempty"`
+	Date      time.Time      `json:"Date" pg:"default:now(), pk, notnull"`
+	Content   string         `json:"Content" pg:", notnull"`
+	UpdatedAt types.NullTime `json:"UpdatedAt,omitempty"`
 }
