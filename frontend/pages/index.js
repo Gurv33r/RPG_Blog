977fd28e6d9c37b6b9bd471b02948a24893dda80
Gurveer Singh <70867602+Gurv33r@users.noreply.github.com>
2022-01-26 00:27:07 -0800

    final cpmmit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import dotenv from 'dotenv'
 
 import styles from '../styles/Home.module.css'
 import postStyles from '../styles/Post.module.css';
@@ -11,14 +10,14 @@ function jsxify(post, i) {
   const editDate = post.UpdatedAt
   const publishDate = post.Date.split('T')[0]
   const jsx = [ 
-  <div className={postStyles.publishDate}>
+  <div key={1} className={postStyles.publishDate}>
     {publishDate}
     </div>,
-    <p className={postStyles.contentHome}>{post.Content.substring(0,10) + '...'}</p>,
-    <Link href={`/${publishDate}`}><u className={postStyles.link}>See full post</u></Link>
+    <p key={2} className={postStyles.contentHome}>{post.Content.substring(0,10) + '...'}</p>,
+    <Link key={3} href={`/${publishDate}`}><u className={postStyles.link}>See full post</u></Link>
   ]
   if (editDate){
-    jsx.push(<div className={postStyles.editDate}>Edited on {editDate.substring(0,10)} at {editDate.substring(11,16)}</div>)
+    jsx.push(<div key={4} className={postStyles.editDate}>Edited on {editDate.substring(0,10)} at {editDate.substring(11,16)}</div>)
   }
   return  (
       <div className={postStyles.wrapper} key={i}>
@@ -30,6 +29,7 @@ function jsxify(post, i) {
 
 export default function Blog({ posts }) {
   const router = useRouter()
+  if(posts){
     return (
       <div className={styles.container}>
         <Head>
@@ -49,9 +49,27 @@ export default function Blog({ posts }) {
       </div>
     )
   }
+  return (
+    <div className={styles.container}>
+        <Head>
+          <title>RPG Blog</title>
+          <meta name="description" content="Blog app made using React, Go, and PostgreSQL" />
+        </Head>
+        <main className={styles.main}>
+          <button className={postStyles.btn} onClick={event => {
+            event.preventDefault()
+            router.push('/new')
+          }}>New Post</button>
+          <p>No posts so far!</p>
+        </main>
+        <footer className={styles.footer}>
+          A Gurveer Singh Production &copy;
+        </footer>
+      </div>
+  )
+}
 
 export async function getStaticProps() {
-    dotenv.config()
     const res = await fetch(`${process.env.BACKEND_URL}/all`)
     const posts = await res.json();
     return {
